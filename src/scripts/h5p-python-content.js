e9d8512de5b5eef7e1e4afddb083f1eadb151b3c
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-04-20 20:26:42 +0200

    Update h5p-python-content.js

--- src/scripts/h5p-python-content.js
+++ src/scripts/h5p-python-content.js
@@ -18,8 +18,8 @@ export default class PythonContent {
     this.randomApiKey = (parseInt(Math.random() * 58786559 + 1679616)).toString(36); // generate a string between 10000 and ZZZZZ
     this.setupApi();
 
-    this.executeBeforeCode = this.getInjectedApi() + '\n' + CodeMirror.H5P.decode(this.params.advancedOptions.executeBeforeCode) + '\n';
-    this.executeAfterCode = '\n' + 'h5p_api_loader_' + this.randomApiKey + '()\n' + CodeMirror.H5P.decode(this.params.advancedGrading.gradingCode);
+    this.executeBeforeCode = CodeMirror.H5P.decode(this.params.executeBeforeCode) + '\n';
+    this.executeAfterCode = '\n' + this.getInjectApiCode() + '\n' + CodeMirror.H5P.decode(this.params.grading.gradingCode);
 
     this.content = document.createElement('div');
     this.content.classList.add('h5p-python-content');
@@ -497,11 +497,8 @@ export default class PythonContent {
     return codeToRun;
   }
 
-  getInjectedApi() {
-    let api = '';
-    //api += 'class H5P :';
-    //api += '  ';
-    return api;
+  getInjectApiCode() {
+    return 'h5p_api_loader_' + this.randomApiKey + '()';
   }
 
   setupApi() {
